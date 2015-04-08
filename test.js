9b3f17ee6867ff7cc81f43b825a192e025e49be5
Richard Janicek <r@janicek.co>
2015-04-07 22:18:42 -0400

    updated dependencies

--- test.js
+++ test.js
@@ -5,7 +5,6 @@
 
 'use strict';
 
-var _ = require('lodash');
 var glob = require('glob');
 var nodeunit = require('nodeunit');
 var reporter = nodeunit.reporters.default;
@@ -13,10 +12,6 @@ var reporter = nodeunit.reporters.default;
 var pattern = process.argv.length === 3 ? process.argv[2] : '';
 
 glob('scripts/**/*' + pattern + '*_test.js', function (er, files) {
-
-    // There might be a way to do this in the glob pattern, but I couldn't find it.
-    files = _.filter(files, function (file) { return file.indexOf('/node_modules/') === -1; });
-
     reporter.run(files, null, function () {
         process.exit();
     });
